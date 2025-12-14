3851939c44f1d8c8269ee58b33c2c4e03c92faf7
sq422 <3120614845@qq.com>
2025-12-14 22:54:40 +0800

    no message

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -5,5 +5,5 @@ export default defineConfig({
   // 填写你的 GitHub Pages 站点地址（如：https://<你的用户名>.github.io）
   site: 'https://sq422.github.io',
   // 若仓库名不是 <用户名>.github.io，需填写仓库名（如：/blog2）；否则注释掉这一行
-  base: '/blog2',
+  //base: '/blog2',
 });
